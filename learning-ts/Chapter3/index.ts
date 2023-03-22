00851ce01ecb17fd289c5120d542524be8b1f1ff
yeongi <cbg5255@gmail.com>
2023-03-22 14:49:17 +0900

    docs : readme.md 최신화

--- learning-ts/Chapter3/index.ts
+++ learning-ts/Chapter3/index.ts
@@ -1,57 +1,57 @@
-let isNum = Math.random() > 0.5 ? "123" : 10;
-
-//ok
-isNum.toString();
-//not ok
-isNum.toFixed();
-
-//literal
-const myName = "JangUiYoung";
-
-//not literal
-let yourName = "KimYoungHoon";
-
-let school: "DSU" | null;
-
-school = null; //ok
-school = "DSU"; //ok
-school = "BDU"; //no ok
-
-let myScore: 233 | undefined;
-
-if (myScore) console.log(myScore.toFixed); // type : number
-
-console.log(myScore.toFixed); // type : number | undefined
-
-// let number = Math.random() > 0.5 ? "123" : 10;
+// let isNum = Math.random() > 0.5 ? "123" : 10;
 
 // //ok
-// if (number === 10) number.toFixed(1);
-
+// isNum.toString();
 // //not ok
-// number.toFixed(10);
+// isNum.toFixed();
 
-let myNumber = Math.random() > 0.5 ? "123" : 10;
+// //literal
+// const myName = "JangUiYoung";
 
-if (typeof myNumber === "number") {
-  myNumber.toFixed(1);
-} else {
-  myNumber.toString();
-}
+// //not literal
+// let yourName = "KimYoungHoon";
 
-typeof myNumber === "string" ? myNumber.toString() : myNumber.toFixed(1);
+// let school: "DSU" | null;
 
-const philosopher = "Hysteria";
+// school = null; //ok
+// school = "DSU"; //ok
+// school = "BDU"; //no ok
 
-let lifespan: number | "ongoing" | "uncertain";
+// let myScore: 233 | undefined;
 
-lifespan = 10;
-lifespan = "ongoing";
-lifespan = "uncertain";
+// if (myScore) console.log(myScore.toFixed); // type : number
 
-let myFreind: string;
-const me = "JUY";
-//ok
-myFreind = "KYM";
-//ok
-myFreind = me;
+// console.log(myScore.toFixed); // type : number | undefined
+
+// // let number = Math.random() > 0.5 ? "123" : 10;
+
+// // //ok
+// // if (number === 10) number.toFixed(1);
+
+// // //not ok
+// // number.toFixed(10);
+
+// let myNumber = Math.random() > 0.5 ? "123" : 10;
+
+// if (typeof myNumber === "number") {
+//   myNumber.toFixed(1);
+// } else {
+//   myNumber.toString();
+// }
+
+// typeof myNumber === "string" ? myNumber.toString() : myNumber.toFixed(1);
+
+// const philosopher = "Hysteria";
+
+// let lifespan: number | "ongoing" | "uncertain";
+
+// lifespan = 10;
+// lifespan = "ongoing";
+// lifespan = "uncertain";
+
+// let myFreind: string;
+// const me = "JUY";
+// //ok
+// myFreind = "KYM";
+// //ok
+// myFreind = me;
